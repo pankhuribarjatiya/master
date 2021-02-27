eb289789599209455394225aebd614bbb05daede
dishapatel7195 <70923120+dishapatel7195@users.noreply.github.com>
2021-02-26 19:24:48 -0800

    modifided app-routing

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 import { RestaurantListsComponent } from './components/restaurant/restaurant-lists/restaurant-lists.component';
 import { RestaurantMenuComponent } from './components/restaurant/restaurant-menu/restaurant-menu.component';
-import { CartComponent } from './components/restaurant/cart/cart.component';
+//import { CartComponent } from './components/restaurant/cart/cart.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'restaurant', pathMatch: 'full' },
-  { path: 'restaurantList', component: RestaurantListsComponent },
-  { path: 'restaurantMenu/:RestId/', component: RestaurantMenuComponent },
-  { path: 'cart', component: CartComponent }
-];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  { path: '', redirectTo: 'restaurant', pathMatch: 'full' },
+  { path: 'restaurant', component: RestaurantComponent },
+  { path: 'restaurantList', component: RestaurantListsComponent },
+  { path: 'restaurantMenu', component: RestaurantMenuComponent},
+  //{ path: 'cart', component: CartComponent }
+];
+@NgModule({imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
