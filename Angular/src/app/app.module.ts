dd78b3d99dc3accd50c4b6710531d3f5a09eeed8
Aishwarya Supekar <aish@Aishwaryas-MacBook-Air-2.local>
2021-03-05 16:07:26 -0800

    Login page working

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -45,7 +46,8 @@ import { PageNotFoundComponent } from './components/shared/page-not-found/page-n
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
