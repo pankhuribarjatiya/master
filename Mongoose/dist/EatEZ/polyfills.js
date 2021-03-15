2d2e8fccdb0fe58f3cfd21d0c9a5b96266f0beaf
Pankhuri Barjatiya <pankhuri@Pankhuris-MacBook-Pro.local>
2021-03-14 18:19:31 -0700

    testing

--- Mongoose/dist/EatEZ/polyfills.js
+++ Mongoose/dist/EatEZ/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\ksraj\project\EatEz_SaaS\Angular\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! /Users/pankhuri/Downloads/EatEz_SaaS/Angular/src/polyfills.ts */"hN/g");
 
 
 /***/ }),
